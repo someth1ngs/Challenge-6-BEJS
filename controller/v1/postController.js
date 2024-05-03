034d886925f130289a459df7415b58094c919e2c
Dewangga <hamadakinose@gmail.com>
2024-05-03 17:54:31 +0700

    fix and added post on Post

--- controller/v1/postController.js
+++ controller/v1/postController.js
@@ -1,14 +1,70 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const path = require("path");
-const imageKit = require("../libs/imagekit");
+const imageKit = require("../../libs/imagekit");
 
 module.exports = {
-    post: async (req, res, next) => {
-        try {
-            
-        } catch (error) {
-            next(error)
-        }
+  post: async (req, res, next) => {
+    try {
+      let { title, description } = req.body;
+
+      if (!req.file || !title || !description) {
+        return res.status(400).json({
+          status: false,
+          message: "Input must be required",
+          data: null,
+        });
+      }
+
+      let strFile = req.file.buffer.toString("base64");
+
+      let { url, fileId } = await imageKit.upload({
+        fileName: Date.now() + path.extname(req.file.originalname),
+        file: strFile,
+        folder: "/challenge6/images",
+      });
+
+      const post = await prisma.post.create({
+        data: {
+          title,
+          description,
+          post_url: url,
+          user_id: req.user.id,
+          post_id: fileId,
+        },
+      });
+      delete post.image_id;
+
+      return res.status(201).json({
+        status: true,
+        message: "Post Successfully",
+        data: { post },
+      });
+    } catch (error) {
+      next(error);
     }
-}
+  },
+
+  index: async (req, res, next) => {
+    try {
+      const { search } = req.query;
+
+      const Post = await prisma.post.findMany({
+        where: { title: { contains: search, mode: "insensitive" } },
+      });
+
+      Post.forEach((image) => {
+        delete image.image_id;
+        delete image.user_id;
+      });
+
+      res.status(200).json({
+        status: true,
+        message: "success",
+        data: Post,
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+};
--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,4 +1,8 @@
 const router = require("express").Router();
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yaml");
+const fs = require("fs");
+const path = require("path");
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET_KEY } = process.env;
 const { image } = require("../../libs/multer.js");
@@ -32,14 +36,30 @@ let restrict = (req, res, next) => {
 
 // Required Controller
 const userController = require("../../controller/v1/userController.js");
+const postController = require("../../controller/v1/postController.js");
+
+// Directory Documentation API
+const swagger_path = path.resolve(__dirname, "../../api-docs.yaml");
+const file = fs.readFileSync(swagger_path, "utf-8");
+
+// API Docs
+const swaggerDocument = YAML.parse(file);
+router.use(
+  "/api-docs",
+  swaggerUI.serve,
+  swaggerUI.setup(swaggerDocument)
+);
 
 // API Users //
-router.post("/api/v1/users", userController.register);
-router.get("/api/v1/users",  userController.index);
-router.put("/api/v1/users/:id", restrict, image.single("file"), userController.update);
+router.post("/users", userController.register);
+router.get("/users",  userController.index);
+router.put("/users/:id", restrict, image.single("file"), userController.update);
 
 // LOGIN AND AUTH API
-router.post("/api/v1/auth/login", userController.login);
-router.get("/api/v1/auth/authenticate", restrict, userController.auth);
+router.post("/auth/login", userController.login);
+router.get("/auth/authenticate", restrict, userController.auth);
+
+// API POST //
+router.post("/posts", restrict, image.single('file'), postController.post);
 
 module.exports = router;
