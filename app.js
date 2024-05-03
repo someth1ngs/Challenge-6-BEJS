9551d8ebe10a6e6cded5105f224acc3642adeae0
Dewangga <hamadakinose@gmail.com>
2024-05-03 18:46:15 +0700

    added some redirect

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const morgan = require("morgan");
 const cors = require("cors");
 const router = require("./routes/v1");
 
+app.get('/', (req, res) => {
+    const baseUrl = req.baseUrl;
+    res.redirect(`${baseUrl}api/v1/api-docs/`);
+});
+
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
